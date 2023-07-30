7a792d5ac398bf50f7908bafab073925e38c526c
Gerald Todd <beingforthebenefit@gmail.com>
2023-07-30 10:54:06 -0700

    Add weather emoji map

---
+++ src/components/WeatherEmojis.ts
@@ -0,0 +1,25 @@
+// WeatherEmojis.ts
+
+type EmojiMap = {
+  [key: string]: string;
+};
+
+const WeatherEmojis: EmojiMap = {
+  'Thunderstorm': '⛈️',
+  'Drizzle': '🌧️',
+  'Rain': '☔',
+  'Snow': '❄️',
+  'Clear': '☀️',
+  'Clouds': '☁️',
+  'Mist': '🌫️',
+  'Smoke': '💨',
+  'Haze': '🌫️',
+  'Dust': '💨',
+  'Fog': '🌫️',
+  'Sand': '💨',
+  'Ash': '💨',
+  'Squall': '🌬️',
+  'Tornado': '🌪️',
+};
+
+export default WeatherEmojis;
