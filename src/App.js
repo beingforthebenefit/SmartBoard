f8efb7eacd8f8763974a4b01e6a32ab4b445ec18
Gerald Todd <beingforthebenefit@gmail.com>
2023-07-30 11:11:51 -0700

    Implement Weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import { NextUIProvider } from '@nextui-org/react';
+import { Grid, NextUIProvider, Spacer, Col } from '@nextui-org/react';
 import Clock from './components/Clock';
+import Weather from './components/Weather';
 
 function App() {
   return (
     <NextUIProvider>
-      <Clock />
+      <Grid.Container gap={2} justify="center">
+        <Col span={6} justify="center">
+          <Clock />
+        </Col>
+        <Col span={6}>
+          <Weather />
+        </Col>
+        <Col span={12}>
+          <Spacer y={30} />
+        </Col>
+      </Grid.Container>
     </NextUIProvider>
   );
 }
