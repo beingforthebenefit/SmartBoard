9b0a451389c4d2b9b7b7b35d7a5c2147bb6acccb
Gerald Todd <beingforthebenefit@gmail.com>
2023-07-30 11:20:49 -0700

    Implement BackgroundImage

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import './App.css';
 import { Grid, NextUIProvider, Spacer, Col } from '@nextui-org/react';
 import Clock from './components/Clock';
 import Weather from './components/Weather';
+import BackgroundImage from './components/BackgroundImage';
+
+const imageUrls = [
+  'https://upload.wikimedia.org/wikipedia/commons/8/8d/Frog_on_palm_frond.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/3/3b/Downtown_Chicago_Marina_City_CornCob_Sky_Scrapers.jpg',
+  'https://i.insider.com/6340783eb3e94d0019781b4e?width=1300&format=jpeg&auto=webp',
+];
+const imageInterval = 5; // change image every 5 minutes
 
 function App() {
   return (
     <NextUIProvider>
+      <BackgroundImage imageUrls={imageUrls} interval={imageInterval} />
       <Grid.Container gap={2} justify="center">
         <Col span={6} justify="center">
           <Clock />
         </Col>
         <Col span={6}>
           <Weather />
-        </Col>
-        <Col span={12}>
-          <Spacer y={30} />
         </Col>
       </Grid.Container>
     </NextUIProvider>
