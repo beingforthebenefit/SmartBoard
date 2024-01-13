957b6e41be6f198b683619aa08d4a7b3449fbed0
Gerald Todd <beingforthebenefit@gmail.com>
2024-01-13 10:38:00 -0800

    Rename App.js to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
-import './App.css';
-import { Grid, NextUIProvider, Col, Row, Divider } from '@nextui-org/react';
-import Clock from './components/Clock';
-import Weather from './components/Weather';
-import BackgroundImage from './components/BackgroundImage';
-import SoberCounter from './components/SoberCounter';
-import Calendar from './components/Calendar';
-import { TopGradient, BottomGradient } from './components/Gradients';
+import './App.css'
+import { Grid, NextUIProvider, Col, Row, Divider } from '@nextui-org/react'
+import Clock from './components/Clock'
+import Weather from './components/Weather'
+import BackgroundImage from './components/BackgroundImage'
+import SoberCounter from './components/SoberCounter'
+import Calendar from './components/Calendar'
+import { TopGradient, BottomGradient } from './components/Gradients'
+import React from 'react'
 
 const imageUrls = [
   'https://upload.wikimedia.org/wikipedia/commons/8/8d/Frog_on_palm_frond.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/3/3b/Downtown_Chicago_Marina_City_CornCob_Sky_Scrapers.jpg',
   'https://i.insider.com/6340783eb3e94d0019781b4e?width=1300&format=jpeg&auto=webp',
-];
-const imageInterval = .1; // change image every 5 minutes
+]
+const imageInterval = .1 // change image every 5 minutes
 const icsUrls = [
   "https://p55-caldav.icloud.com/published/2/MTAwMTA1MDY5OTEwMDEwNcR_tgzPDCleovZiKwbmEvqLNmZR2Q-8uMi3IcW12_UH",
   "https://p40-caldav.icloud.com/published/2/Mjc4NDM4OTA3Mjc4NDM4OYtGMLx1oo-hpjvxzsl8_VcgsVNXw4ETUlzBSsVLaG9u_nqSNqTBLJo3W2DiyRgi-2B2DgS80R1FUz-CSGuOylI"
-];
+]
 
 function App() {
   return (
@@ -42,7 +43,7 @@ function App() {
       </Grid.Container>
       <BottomGradient />
     </NextUIProvider>
-  );
+  )
 }
 
-export default App;
+export default App
