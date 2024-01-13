87e2459e3f7d47ed41346b46a03d6055a7f7c095
Gerald Todd <beingforthebenefit@gmail.com>
2024-01-13 10:37:08 -0800

    Update sobercounter to use years and months

--- src/components/SoberCounter.tsx
+++ src/components/SoberCounter.tsx
@@ -12,37 +12,55 @@ const SoberCounter = () => {
     return () => clearInterval(timer);
   }, [soberDate]);
 
-  const seconds = Math.floor(timePassed / 1000) % 60;
-  const minutes = Math.floor(timePassed / (1000 * 60)) % 60;
+  const calculateTime = (startDate: Date, currentDate: Date) => {
+    let years = currentDate.getFullYear() - startDate.getFullYear();
+    let months = currentDate.getMonth() - startDate.getMonth();
+    let days = currentDate.getDate() - startDate.getDate();
+
+    if (days < 0) {
+      months--;
+      const previousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 0);
+      days += previousMonth.getDate();
+    }
+
+    if (months < 0) {
+      years--;
+      months += 12;
+    }
+
+    return { years, months, days };
+  };
+
+  const { years, months, days } = calculateTime(soberDate, new Date());
+
   const hours = Math.floor(timePassed / (1000 * 60 * 60)) % 24;
-  const days = Math.floor(timePassed / (1000 * 60 * 60 * 24));
 
   return (
     <div>
       <Text h3 style={{textAlign: 'center'}}>Sober Time</Text>
       <Grid.Container gap={2} justify="center">
+      <Grid xs={24} sm={3}>
+          <Card>
+            <Text h4 style={{textAlign: 'center'}}>{years}</Text>
+            <Text style={{textAlign: 'center'}}>{years > 1 ? 'Years' : 'Year'}</Text>
+          </Card>
+        </Grid>
+        <Grid xs={24} sm={3}>
+          <Card>
+            <Text h4 style={{textAlign: 'center'}}>{months}</Text>
+            <Text style={{textAlign: 'center'}}>{months > 1 ? 'Months' : 'Month'}</Text>
+          </Card>
+        </Grid>
         <Grid xs={24} sm={3}>
           <Card>
             <Text h4 style={{textAlign: 'center'}}>{days}</Text>
-            <Text style={{textAlign: 'center'}}>Days</Text>
+            <Text style={{textAlign: 'center'}}>{days > 1 ? 'Days' : 'Day'}</Text>
           </Card>
         </Grid>
         <Grid xs={24} sm={3}>
           <Card>
             <Text h4 style={{textAlign: 'center'}}>{hours}</Text>
-            <Text style={{textAlign: 'center'}}>Hours</Text>
-          </Card>
-        </Grid>
-        <Grid xs={24} sm={3}>
-          <Card>
-            <Text h4 style={{textAlign: 'center'}}>{minutes}</Text>
-            <Text style={{textAlign: 'center'}}>Minutes</Text>
-          </Card>
-        </Grid>
-        <Grid xs={24} sm={3}>
-          <Card>
-            <Text h4 style={{textAlign: 'center'}}>{seconds}</Text>
-            <Text style={{textAlign: 'center'}}>Seconds</Text>
+            <Text style={{textAlign: 'center'}}>{hours > 1 ? 'Hours' : 'Hours'}</Text>
           </Card>
         </Grid>
       </Grid.Container>
